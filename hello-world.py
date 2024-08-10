49917a048089516a8702199e7d2c2fead1be6582
alemndev <140274341+alemndev@users.noreply.github.com>
2024-08-10 06:06:47 -0500

    02 - hello world

---
+++ hello-world.py
@@ -0,0 +1,14 @@
+# takataka
+"""
+takataka multiline
+"""
+'''
+takataka multiline
+'''
+print("Hola Python")
+print('Hola Python')
+
+print(type("takataka str"))  # str
+print(type(1))  # int
+print(type(1.99))  # float
+print(type(False))  # bool
