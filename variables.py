c14b8850eb28c9d71dea224ccb2d14b6f69dafaf
alemndev <140274341+alemndev@users.noreply.github.com>
2024-08-10 06:53:06 -0500

    03 - variables

---
+++ variables.py
@@ -0,0 +1,10 @@
+# Variables
+
+message = "hello world"
+year = 2024
+i_like_anime = True
+print(message, year, i_like_anime)
+
+message = "other hello world"
+year = 2077
+print(message, year, i_like_anime)
